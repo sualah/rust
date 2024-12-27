31e11fa374d277fcaa41c40693ff2a883f7c2855
sualah <sualahabdullai@gmail.com>
2024-12-27 23:40:21 +0000

    solved difference-of-square

--- difference-of-squares/src/lib.rs
+++ difference-of-squares/src/lib.rs
@@ -1,11 +1,15 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    todo!("square of sum of 1...{n}")
+    //using gaus's formula
+    (n * (n + 1) / 2).pow(2)
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    todo!("sum of squares of 1...{n}")
+     (1..=n).map(|x| x * x).sum()
+
+    //using a formula
+    //n * (n + 1) * (2 * n + 1) / 6
 }
 
 pub fn difference(n: u32) -> u32 {
-    todo!("difference between square of sum of 1...{n} and sum of squares of 1...{n}")
+    square_of_sum(n) - sum_of_squares(n)
 }
