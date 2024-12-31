60525fc72fe26c1501d5e0f2468c0a61d72f68c8
sualah <sualahabdullai@gmail.com>
2024-12-31 23:39:03 +0000

    solved prime-factors

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    todo!("This should calculate the prime factors of {n}")
+    let mut prime_factors:Vec<u64> = Vec::new();
+    let mut n_num = n;
+    let mut divisor = 2;
+
+    while n_num > 1 {
+        while n_num % divisor == 0 {
+            prime_factors.push(divisor);
+            n_num /= divisor;
+        }
+        divisor += 1;
+    }
+
+    prime_factors
 }
+
